67ee2a6d06d16181b276ccd14e22931f88fac30c
Eren Erogullari <erenogullari@gmail.com>
2026-07-20 16:43:22 +0200

    update ci workflow

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, test } from "vitest";
 
-const person = {
-  isActive: true,
-  age: 32,
-};
+// const person = {
+//   isActive: true,
+//   age: 32,
+// };
+
+const person = undefined;
 
 describe("person", () => {
   test("person is defined", () => {
